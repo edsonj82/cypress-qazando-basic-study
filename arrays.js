5ef4267e0ad6547c4614c625a461e3b7bc4a6be1
Edson José dos Santos <edsonj82@outlook.com>
2024-01-25 20:18:26 -0300

    Studying arrays in JS

---
+++ arrays.js
@@ -0,0 +1,19 @@
+const names = ['Edson', 'Tamires', 'Izabel','Pedro', 41, 35, 74, 80]
+//index: 6
+//elements: 7
+
+//console.log(names)
+console.log(names[0])
+
+//push: adicionando novos valores ao meu array
+var age  = []
+age.push(10)
+age.push(20)
+age.push(30)
+
+console.log(age)
+console.log(age[0])
+
+//undefined: it does not exist
+//empty: exist, but its value is empty
+//null: it exists, but it is null
