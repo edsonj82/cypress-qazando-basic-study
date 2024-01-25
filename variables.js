8073231ce48e09f2f3c173e0442a664279bc1482
Edson José dos Santos <edsonj82@outlook.com>
2024-01-25 19:45:59 -0300

    Studying type off variables in JS

---
+++ variables.js
@@ -0,0 +1,18 @@
+// o tipo 'var' são variáveis que podem ser alteradas
+var name
+name = "Edson" //string: texto
+name = 50 //int: numero
+name = false //boolean: true/false
+console.log(name)
+
+// o tipo 'let' são variáveis que podem ser alteradas
+let year = 90;
+idade = 41
+console.log(idade);
+
+// o tipo 'const' são variáveis que NÃO podem serEM modificadas
+const address = 'Rua Xpto'
+//address = 'Rua X'
+console.log(address);
+
+//node variable.js
