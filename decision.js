90c8f44ae2bfb8c658a8d319feb5afd6d370c973
Edson José dos Santos <edsonj82@outlook.com>
2024-01-25 21:42:28 -0300

    Studying decisions (if and else) in JS

---
+++ decision.js
@@ -0,0 +1,14 @@
+var age = 10
+
+if(age == 10)
+    console.log('It is equal 10')
+else
+    console.log('It is not equal 10')
+
+console.log('------------------------------------')
+
+var num = true
+if(num)
+    console.log('Validate')
+else
+    console.log('Not validate')
